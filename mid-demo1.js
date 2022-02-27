0777d8a76c15aa15bd5580733a414e14f970a3e6
Razerbeam115 <99705946+Razerbeam115@users.noreply.github.com>
2022-02-28 01:33:47 +0800

    Add files via upload

---
+++ mid-demo1.js
@@ -0,0 +1,29 @@
+//Activity Name: mid-demo1.js
+//Author Name: Robbie Joseph Mabanta
+//Section and code: WD-202
+
+var express = require('Express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get("/index.html", function(req, res){
+    res.sendFile(__dirname + "/" + "index.html");
+})
+
+app.get("/process_get", function(req, res){
+    response ={
+        first_name:req.query.first_name,
+        last_name:req.query.last_name
+    };
+
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
+
+var Server = app.listen(5000,'localhost', function(){
+    var host = Server.address().address
+    var port = Server.address().port
+
+    console.log("Example app listening at http://%s:%s",host,port)
+});
